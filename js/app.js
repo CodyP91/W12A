705851cfe125f6f6479b11b06429fd0729bbe880
Cody <cjoel91@hotmail.com>
2023-04-05 16:55:01 -0600

    W12A Axios1 Updates

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
-var getActivityButton = document.getElementById("getActivityButton");
-var activityElement = document.getElementById("activity");
+document.getElementById('loadImage').addEventListener('click', function() {
+  var dogImageContainer = document.getElementById('dogImageContainer');
 
-getActivityButton.addEventListener("click", function() {
- 
-  activityElement.textContent = "Loading...";
-  axios.get("https://www.boredapi.com/api/activity/")
-    .then(function(response) {
-  
-      activityElement.textContent = response.data.activity;
-    })
-    .catch(function(error) {
-  
-      activityElement.textContent = "Sorry, there was an error getting your activity recommendation.";
-      console.log(error);
-    });
+  // Display loading message
+  dogImageContainer.innerHTML = '<p>Loading...</p>';
+
+  // Make the API call using Axios
+  axios.request({
+      method: 'get',
+      url: 'https://dog.ceo/api/breeds/image/random'
+  })
+  .then(function(response) {
+      // Handle success
+      var imageUrl = response.data.message;
+      dogImageContainer.innerHTML = '<img src="' + imageUrl + '" alt="Random Dog Image" />';
+  })
+  .catch(function(error) {
+      // Handle error
+      dogImageContainer.innerHTML = '<p>An error occurred while fetching the image. Please try again later.</p>';
+  });
 });
